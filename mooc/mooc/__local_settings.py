d72bb7a4287d2a36a0c9b0ce3180fab220a11b10
Barry Peddycord III <ishara@isharacomix.org>
2013-02-11 22:41:00 -0500

    Started the Django project. Time to go down the rabbit hole.

---
+++ mooc/mooc/__local_settings.py
@@ -0,0 +1,48 @@
+# Local settings for mooc project.
+LOCAL_SETTINGS = True
+from settings import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.',
+        # Or path to database file if using sqlite3.
+        'NAME': '',
+        # Not used with sqlite3.
+        'USER': '',
+        # Not used with sqlite3.
+        'PASSWORD': '',
+        # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '',
+        # Set to empty string for default. Not used with sqlite3.
+        'PORT': '',
+    }
+}
+
+# If you have Redis installed, copy this.
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.cache.RedisCache',
+        'LOCATION': '127.0.0.1:6379:5',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+        }
+    }
+}
+
+# If you don't have Redis installed, copy this.
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'ncsulug'
+    }
+}
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = ''
+
+if DEBUG:
+    # Show emails in the console during developement.
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
