8b49896d429d52ef1bf56525ffa749acd02f5981
Barry Peddycord III <ishara@isharacomix.org>
2013-02-14 10:32:16 -0500

    Changed the URL scheme for the textbook to a slug. No autocorrect for capital letters.

--- mooc/textbook/urls.py
+++ mooc/textbook/urls.py
@@ -6,6 +6,6 @@ from textbook import views
 # Textbook URL pattern.
 urlpatterns = patterns('',
     url(r'^$', views.view_page, name='textbook'),
-    url(r'^(?P<page>[\w-]+)/', views.view_page, name='textbook_page'),
+    url(r'^(?P<page>[a-z0-9-]+)/', views.view_page, name='textbook_page'),
 )
 
