2a906d6f480e8f1a396f7257d322513b523dab11
Barry Peddycord III <ishara@isharacomix.org>
2013-02-19 21:39:59 -0500

    Added the quizchallenge and made it pretty reasonable. Next up, the view for quizzes.

--- mooc/lessons/urls.py
+++ mooc/lessons/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.base import TemplateView
 
-from textbook import views
+from lessons import views
 
 # Textbook URL pattern.
 urlpatterns = patterns('',
-    url(r'^$', views.view_page, name='textbook'),
-    url(r'^(?P<page>[^/]+)/$', views.view_page, name='textbook_page'),
-    url(r'^search/(?P<query>[^/]+)$', views.find_pages, name='textbook_search'),
+    url(r'^lesson/$', views.test, name='lesson'),
+    url(r'^challenge/$', views.test, name='challenge'),
 )
 
--- mooc/lessons/views.py
+++ mooc/lessons/views.py
@@ -1,3 +1,17 @@
 # Create your views here.
 
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.http import (HttpResponse, HttpResponseRedirect,
+                         HttpResponseForbidden, Http404)
+from django.shortcuts import render, redirect
 
+from textbook.models import Page
+from lessons.models import Lesson
+
+
+# 
+def test(request):
+    k = Lesson.objects.get(shortname="testy")
+    return HttpResponse( k.challenge.dummychallenge.name )
+    
