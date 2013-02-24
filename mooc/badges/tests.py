bd72f442c8a841f0e23838f7195fb0de7b33ef20
Barry Peddycord III <ishara@isharacomix.org>
2013-02-23 19:45:56 -0500

    Actually did some unit testing!

--- mooc/badges/tests.py
+++ mooc/badges/tests.py
@@ -1,8 +1,5 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+Tests for the badges.
 """
 
 from django.test import TestCase
@@ -13,6 +10,33 @@ from students.models import Student
 class BadgeModelTest(TestCase):
     def test_held_by(self):
         """
-        Tests that 1 + 1 always equals 2.
+        This tests the 'held_by' function of the badge model, despite it
+        being against the design philosophy for the badge to store who it
+        is awarded to rather than the user asserting the badge for themself.
         """
-        self.assertEqual(1 + 1, 2)
+        
+        # Create the test badge.
+        b = Badge( name = "superbadge",
+                   shortname = "superbadge",
+                   description = "superbadge",
+                   graphic = "superbadge" )
+        b.save()
+        
+        # Create some test users.
+        u1 = User( username="kellydoctor", email="kelly@doctor.com" )
+        u1.save()
+        s1 = Student( user=u1 )
+        s1.save()
+        u2 = User( username="andyprogrammer", email="andy@programmer.com" )
+        u2.save()
+        s2 = Student( user=u2 )
+        s2.save()
+        
+        # Award the badge to s1
+        b.awarded_to.add( s1 )
+        b.save()
+        
+        # Now run the tests.
+        self.assertTrue( b.held_by( s1 ) )
+        self.assertFalse( b.held_by( s2 ) )
+        
