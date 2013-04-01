72eb28640fd65c7d77d69e87c949babd1bff4949
Barry Peddycord III <ishara@isharacomix.org>
2013-04-01 10:57:06 -0400

    I hate working with models.

--- mooc/assembler/models.py
+++ mooc/assembler/models.py
@@ -9,9 +9,7 @@ from world.models import BaseChallenge
 class AssemblyChallenge(BaseChallenge):
     kernal      = models.BooleanField("kernal", default=False,
                         help_text="The kernal for this challenge.")
-                        
-    def __unicode__(self):
-        return u'AssemblyChallenge %d'%(self.id)
+    #TODO - we need to map this to functions that will do the evaluation.
 
 
 # The form submitted when an assembly challenge is attempted. These challenge
